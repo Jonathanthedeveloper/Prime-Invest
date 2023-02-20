0e4439889a0141113aab6ba016bf0e2f5ffd81f8
jonathan <82114500+Jonathanthedeveloper@users.noreply.github.com>
2023-02-20 15:21:08 +0100

    added a user model

--- services/user.service.js
+++ services/user.service.js
@@ -2,5 +2,16 @@ const User = require('../models/user.model');
 
 class UserService {
 
-    
+    // general Crud Operations
+
+    // create 
+    async create(userData) {
+        return await User.create(userData);
+    }
+
+    // update
+    async update(filter, updateData) {
+        return await User.findOneAndUpdate
+    }
+
 }
