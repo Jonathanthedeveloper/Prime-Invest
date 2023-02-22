de37bd683a7d7eb053253bb500928dfc470764d5
ZedPriest <Charleschukwuemeka47@gmail.com>
2023-02-22 07:43:50 +0100

    Add some frontend pages

--- routes/rootRoute.route.js
+++ routes/rootRoute.route.js
@@ -1,35 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-
- //Importing routes
-const aboutRoute = require('./about.route');
-const contactRoute = require('./contact.route');
-const dashboardRoute = require('./dashboard.route');
-const loginRoute = require('./login.route');
-const faqRoute = require('./faq.route');
-const forgotPasswordRoute = require('./forgot-password.route');
-const servicesRoute = require('./service.route');
-const indexRoute = require('./index.route');
-const policyRoute = require('./policy.route');
-const registerRoute = require('./register.route');
-const notFoundRoute = require('./notfound.route');
-
+//Importing routes
+const aboutRoute = require("./about.route");
+const contactRoute = require("./contact.route");
+const dashboardRoute = require("./dashboard.route");
+const loginRoute = require("./login.route");
+const faqRoute = require("./faq.route");
+const forgotPasswordRoute = require("./forgot-password.route");
+const servicesRoute = require("./service.route");
+const indexRoute = require("./index.route");
+const policyRoute = require("./policy.route");
+const registerRoute = require("./register.route");
+const notFoundRoute = require("./notfound.route");
+const depositRoute = require("./deposit.route");
+const checkout = require("./checkout.route");
+const invest = require("./invest.route");
+const withdraw = require("./withdraw.route");
+const history = require("./history.route");
 
 // configuring routes
-router.use('/', indexRoute);
-router.use('/aboutUs', aboutRoute);
-router.use('/contact', contactRoute);
-router.use('/dashboard', dashboardRoute);
-router.use('/login', loginRoute);
-router.use('/faq', faqRoute);
-router.use('/forgot-password', forgotPasswordRoute);
-router.use('/services', servicesRoute);
-router.use('/policy', policyRoute);
-router.use('/create', registerRoute);
-router.use('/notfound', notFoundRoute);
-
+router.use("/", indexRoute);
+router.use("/aboutUs", aboutRoute);
+router.use("/contact", contactRoute);
+router.use("/dashboard", dashboardRoute);
+router.use("/login", loginRoute);
+router.use("/faq", faqRoute);
+router.use("/forgot-password", forgotPasswordRoute);
+router.use("/services", servicesRoute);
+router.use("/policy", policyRoute);
+router.use("/create", registerRoute);
+router.use("/notfound", notFoundRoute);
+router.use("/deposit", depositRoute);
+router.use("/checkout", checkout);
+router.use("/invest", invest);
+router.use("/withdraw", withdraw);
+router.use("/history", history);
 
 // exporting router middleware
 module.exports = router;
