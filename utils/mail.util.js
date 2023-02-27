f492a0d5ccafdcaa64eb3ac4dd122510f2bbeec2
jonathan <82114500+Jonathanthedeveloper@users.noreply.github.com>
2023-02-27 11:23:27 +0100

    added withdrawal route

---
+++ utils/mail.util.js
@@ -0,0 +1,52 @@
+const nodemailer = require('nodemailer')
+
+
+// async..await is not allowed in global scope, must use a wrapper
+async function sendMail(options) {
+    try {
+        // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    const testAccount = await nodemailer.createTestAccount();
+  
+    // create reusable transporter object using the default SMTP transport
+    // const transporter = nodemailer.createTransport({
+    //   host: "smtp.ethereal.email",
+    //   port: 587,
+    //   secure: false, // true for 465, false for other ports
+    //   auth: {
+    //     user: testAccount.user, // generated ethereal user
+    //     pass: testAccount.pass, // generated ethereal password
+    //   },
+    // });
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: 'jonathancodes2@gmail.com', // generated ethereal user
+          pass: "Programming18$", // generated ethereal password
+        },
+      });
+
+    console.log(options)
+  
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: options.to, // list of receivers
+      subject: options.subject, // Subject line
+      text: options.text, // plain text body
+      html: options.html || null, // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    } catch (error) {
+       throw error
+    }
+  }
+  
+
+  module.exports = sendMail
